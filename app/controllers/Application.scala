f9488c90c127fe67dfb7416970af7e1b62ad3883
Efe Gürkan YALAMAN <efeyalaman@gmail.com>
2015-01-14 14:58:58 +0200

    index routing changed

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -10,19 +10,22 @@ import utility.AuthAction
 
 object Application extends Controller{
  
-  def index = Action {request => {
-    val cHelper = new ContactDBHelper()
-    val pHelper = new PostDBHelper
-    val list = cHelper.getContactsByUserId(1)
-    val list2 = pHelper.getPostsById(1)
-    list.foreach{println}
-    println("---")
-    list2.foreach{println}
-    Ok(views.html.index("Your new application is ready."))
-  }
-    
-  }
+//  def index = Action {request => {
+//    val cHelper = new ContactDBHelper()
+//    val pHelper = new PostDBHelper
+//    val list = cHelper.getContactsByUserId(1)
+//    val list2 = pHelper.getPostsById(1)
+//    list.foreach{println}
+//    println("---")
+//    list2.foreach{println}
+//    Ok(views.html.index("Your new application is ready."))
+//  }
+//
+//  }
 
+  def index = Action{request =>
+    Redirect("/login")
+  }
 
   def home = AuthAction { request =>
      val id = request.session.get("LoggedUser")
